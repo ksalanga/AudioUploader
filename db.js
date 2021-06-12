4d9544e69f58d68a7fc2379121ab5b4b82c20a64
ksalanga <kennethsalanga@gmail.com>
2021-06-12 01:59:29 -0400

    moved functions from Recordings.js that used Database to db.js to avoid clutter in the recordings directory

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
-const fs = require('fs')
-const user = JSON.parse(fs.readFileSync('./secrets.json')).user
-const pass = JSON.parse(fs.readFileSync('./secrets.json')).pass
+require('dotenv').config()
+const user = process.env.MONGOUSER
+const pass = process.env.MONGOPASS
 const MongoClient = require('mongodb').MongoClient
 const uri = "mongodb+srv://"+user+":"+pass+"@cluster0.tb1nm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 
 var state = {
-  db: null,
+  db: null
 }
 
 exports.connect = function(done) {
@@ -31,4 +31,41 @@ exports.close = function(done) {
       done(err)
     })
   }
+}
+
+exports.storeRecording = async function storeRecording(req) {
+  var database = state.db.db('AudioJungle')
+  var recordings = database.collection('recordings')
+  var audioObject = {
+      name: req.body.name,
+      date: new Date()
+  }
+
+  var dbResponse = await recordings.insertOne(audioObject)
+  
+  var storage = database.collection('storage')
+  await storage.updateOne({}, {$inc: {"size": req.file.size}})
+
+  return dbResponse.insertedId.toString()
+}
+
+exports.deleteRecording = async function deleteRecording(id) {
+  var database = state.db.db('AudioJungle')
+  var recordings = database.collection('recordings')
+  var audioToDelete = {
+      _id: ObjectId(id)
+  }
+
+  var deleteResult = await recordings.deleteOne(audioToDelete)
+  return deleteResult
+}
+
+exports.getStorageSize = async function getStorageSize() {
+  var database = state.db.db('AudioJungle')
+  var storage = database.collection('storage')
+  
+  var storageItem = await storage.findOne({})
+  var size = storageItem.size
+
+  return size
 }
