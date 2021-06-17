0d8babce92160bac63e5b1861028c624876af2bd
ksalanga <kennethsalanga@gmail.com>
2021-06-17 07:33:40 -0400

    Added Rate Limiting Functionality to the Audio Uploading API so that not too many requests are asked from the same source in a given time

--- recordings/recordings.js
+++ recordings/recordings.js
@@ -4,24 +4,64 @@ const multer = require('multer')
 const db = require('../db')
 const { ObjectId } = require('bson')
 const { Storage } = require('@google-cloud/storage')
+const rateLimit = require("express-rate-limit")
 
 const storage = new Storage({projectId: process.env.GCLOUD_PROJECT, credentials: {client_email: process.env.GCLOUD_CLIENT_EMAIL, private_key: process.env.GCLOUD_PRIVATE_KEY}})
 
 const upload = multer({
     storage: multer.memoryStorage(),
+    fileFilter: (req, file, cb) => {
+        if (req.body.name == null || req.body.name == "") {
+            cb(null, false)
+            return cb(new Error('Please provide a name key!'))
+        }
+        if (file.mimetype == "audio/mpeg") {
+            cb(null, true)
+        } else {
+            cb(null, false)
+            return cb(new Error('Only .mp3 format allowed!'))
+        }
+    },
     limits: {
-        // 2 megabytes
-        fileSize: 2 * 1024 * 1024
+        // 1 megabyte
+        fileSize: 1 * 1024 * 1024
     }
 }).single('audio-file')
 
 const bucket = storage.bucket(process.env.GCS_BUCKET) 
 
-router.post('/', (req, res) => {
+const uploadAudioLimiter = rateLimit({
+    windowMs: 30 * 60000, // 30 minutes
+    max: 5, // start blocking after 5 requests
+    handler: function(req, res, /*next*/) {
+        res.status(429).json({
+            limit: req.rateLimit.limit,
+            resetTime: req.rateLimit.resetTime.toLocaleString('default', {
+                dateStyle: 'full',
+                timeStyle: 'long'
+            })
+        })
+    }
+  })
+
+router.post('/', uploadAudioLimiter, (req, res) => {
+    // rate limit ends when the middleware no longer gives a JSON rateLimit key
+    // if (req.rateLimit == null || req.rateLimit == undefined) {
+    //     res.status(429).json({
+    //         msg: 'Too Many Requests',
+    //         limit: req.limit,
+    //         resetTime: req.resetTime.toLocaleString('default', {
+    //             dateStyle: 'full',
+    //             timeStyle: 'long'
+    //         })
+    //     })
+    //     return
+    // }
     upload(req, res, err => {
         if (err instanceof multer.MulterError) {
+            console.log(multer.MulterError)
             res.status(300)
-            res.json({error: 'MulterError'})
+            res.json({error: 'File Size exceeds 1 megabyte'})
             return
         } else if (err) {
             res.status(300)
@@ -91,7 +131,7 @@ async function createRecording(req, res) {
                 var recordings = database.collection('recordings')
                 recordings.updateOne({"_id": ObjectId(id)}, {$set: {"href": publicURL, "size": req.file.size}})
             })
-    
+
             blobStream.end(req.file.buffer)
             res.status(200)
             res.json({message: 'OK'})
